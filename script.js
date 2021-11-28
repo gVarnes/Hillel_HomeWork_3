ac23b489a0f85f03530d426e8edbafe5b4579a03
Vladislav Kolomoiets <vellenger17@gmail.com>
2021-11-28 21:15:18 +0200

    added counting numbers

---
+++ script.js
@@ -0,0 +1,5 @@
+const firstNumber = +prompt('Enter a first number', 0);
+const secondNumber = Number(prompt('Enter a second number', 0));
+const result = firstNumber + secondNumber
+
+alert(`${firstNumber}+${secondNumber}=${result}`)
